23e0289d6090de85a0a3f2990db5639eb9e97cc0
He Peijian <glisxx6@gmail.com>
2018-09-21 15:48:26 +0800

    fix script

--- script.py
+++ script.py
@@ -5,11 +5,6 @@ import requests
 
 url = "http://13.114.55.155/api/luckyBetBenchMark"
 
-luckyNumber = random.randint(0,9)
-amount = random.randint(1,5)
-
-payload = "address=IOST2g5LzaXkjAwpxCnCm29HK69wdbyRKbfG4BQQT7Yuqk57bgTFkY&privKey=319xGCaLZP5D4sAVCEX4LDAMgzaZ3LJiXgCVxB8y1igTmUCkHj6DJRCH4C8myor1P3rZHttFneApzznHqvqqTpiu&betAmount={0}00000000&luckyNumber={1}&gcaptcha=ahaha".format(amount, luckyNumber)
-
 
 headers = {
     'Content-Type': "application/x-www-form-urlencoded",
@@ -18,6 +13,11 @@ headers = {
 }
 
 while True:
+    luckyNumber = random.randint(0,9)
+    amount = random.randint(1,5)
+
+    payload = "address=IOST2g5LzaXkjAwpxCnCm29HK69wdbyRKbfG4BQQT7Yuqk57bgTFkY&privKey=319xGCaLZP5D4sAVCEX4LDAMgzaZ3LJiXgCVxB8y1igTmUCkHj6DJRCH4C8myor1P3rZHttFneApzznHqvqqTpiu&betAmount={0}00000000&luckyNumber={1}&gcaptcha=ahaha".format(amount, luckyNumber)
+
     response = requests.request("POST", url, data=payload, headers=headers)
     print(response.text)
     time.sleep(6)
